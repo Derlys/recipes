7bd398ccd2b65d33b132b7e2341ab2232af5943e
Derlys Dominguez <derlys275@gmail.com>
2023-06-15 17:12:43 +0100

    feat: app recipes with features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Recipe } from './recipe';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'recipes';
+
+  // Array of objects of type Recipe
+  recipes: Recipe[] = [{ name: 'a', ingredients: 'b', description: 'c' }];
+
+  // Object of type recipe
+  recipe: Recipe = {
+    name: '',
+    ingredients: '',
+    description: '',
+  };
+
+  addRecipe(): void {
+    this.recipes.push(this.recipe);
+    this.recipe = { name: '', ingredients: '', description: '' };
+  }
 }
